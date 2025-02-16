8f193ada22005129273fa57a0350d48f12ac3f7e
sigged <sderdeyn@gmail.com>
2025-02-16 23:09:50 +0100

    Create entitybase

---
+++ src/api/Rommelmarkten.Api.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Rommelmarkten.Api.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public virtual Guid Id { get; set; }
+    }
+
+    public abstract class EntityBase<T>
+        where T : notnull
+    {
+        public virtual required T Id { get; set; }
+    }
+}
