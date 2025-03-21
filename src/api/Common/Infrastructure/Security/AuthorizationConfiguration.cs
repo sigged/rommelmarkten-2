bbd29659c7df4e145bd21b0860656978fe2795f1
sigged <sderdeyn@gmail.com>
2025-03-21 20:20:42 +0100

    Register mustbe admin req

--- src/api/Common/Infrastructure/Security/AuthorizationConfiguration.cs
+++ src/api/Common/Infrastructure/Security/AuthorizationConfiguration.cs
@@ -22,7 +22,7 @@ namespace Rommelmarkten.Api.Common.Infrastructure.Security
        
         public static IServiceCollection AddApplicationAuthorization(this IServiceCollection services)
         {
-
+            AuthorizationPolicies.AddAuthorizationPolicy(CorePolicies.MustBeAdmin, policy => policy.Requirements.Add(new MustBeAdminRequirement()));
             AuthorizationPolicies.AddAuthorizationPolicy(CorePolicies.MustBeCreator, policy => policy.Requirements.Add(new MustBeCreatorRequirement()));
             AuthorizationPolicies.AddAuthorizationPolicy(CorePolicies.MustBeCreatorOrAdmin, policy => policy.Requirements.Add(new MustBeCreatorOrAdminRequirement()));
             AuthorizationPolicies.AddAuthorizationPolicy(CorePolicies.MustBeLastModifier, policy => policy.Requirements.Add(new MustBeLastModifierRequirement()));
@@ -38,10 +38,11 @@ namespace Rommelmarkten.Api.Common.Infrastructure.Security
             });
 
             services.AddTransient<IResourceAuthorizationService, ResourceAuthorizationService>();
+
             services.AddSingleton<IAuthorizationHandler, MustBeAdminAuthorizationHandler>();
-            services.AddSingleton<IAuthorizationHandler, MustBeLastModifierAuthorizationHandler>();
             services.AddSingleton<IAuthorizationHandler, MustBeCreatorAuthorizationHandler>();
             services.AddSingleton<IAuthorizationHandler, MustBeCreatorOrAdminAuthorizationHandler>();
+            services.AddSingleton<IAuthorizationHandler, MustBeLastModifierAuthorizationHandler>();
 
             return services;
         }
